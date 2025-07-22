a6985ffc988131d2a8c79568647f9f3c61c6d724
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2025-07-22 11:23:32 +0000

    Added About Component and updated routing to include About page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {path: '', title: "Weather Dashboard", component: DashboardComponent},
-    {path: 'about', title: "Weather Dashboard - About", component: DashboardComponent},
+    {path: 'about', title: "Weather Dashboard - About", component: AboutComponent},
 ];
